a60189dfa9ad384c40f3790e895c095d441231b6
Xan <gmxancrazy24114@gmail.com>
2020-08-22 08:53:17 +0700

    Create panel slide

--- src/Scene/Prepare Scene/Panel.gd
+++ src/Scene/Prepare Scene/Panel.gd
@@ -4,12 +4,27 @@ extends Node2D
 export var direction = "left"
 
 
+var current_hat = ""
+var current_body = "Blue"
+var current_gun = "AR01"
+
+
+
 func _ready():
 	if direction == "left":
 		$Image.texture = load("res://Assets/Scene/Prepare Scene/panel_left.png")
+		$Slide/Image.texture_progress = load("res://Assets/Scene/Prepare Scene/panel_empty_left.png")
 	elif direction == "right":
 		$Image.texture = load("res://Assets/Scene/Prepare Scene/panel_right.png")
+		$Slide/Image.texture_progress = load("res://Assets/Scene/Prepare Scene/panel_empty_right.png")
 		$Button.position.x *= -1.0
+		$Player.scale.x *= -1.0
+	update()
+
+
+func update():
+	$Player/Body.texture = DataList.body_texture[current_body]
+	$Player/Gun.texture = DataList.gun_texture[current_gun]
 
 
 func _on_Clothes_pressed():
@@ -25,6 +40,7 @@ func _on_Button_add_button_down():
 	$Button_remove.show()
 	$Button_add.hide()
 	$Button.show()
+	$Slide/AnimationPlayer.play("disappear")
 
 
 func _on_Button_remove_button_down():
@@ -32,3 +48,4 @@ func _on_Button_remove_button_down():
 	$Button_remove.hide()
 	$Button_add.show()
 	$Button.hide()
+	$Slide/AnimationPlayer.play("appear")
